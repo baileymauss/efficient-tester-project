648f0438de89690a5d4b658ecc73c0fb41060fc3
Bailey Mauss <59042705+baileymauss@users.noreply.github.com>
2021-04-08 12:47:39 -0700

    Update experiments.page.ts

--- app/ionic-form-ui/src/app/experiments/experiments.page.ts
+++ app/ionic-form-ui/src/app/experiments/experiments.page.ts
@@ -6,7 +6,7 @@ import { StorageService, Item } from 'src/app/services/storage.service';
 import { ApiDjangoService } from '../services/api-django.service';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 import {myID} from 'src/app/services/authentication.service';
-import {protocolID} from 'src/app/protocols/protocols.page.ts';
+import {protocolID} from 'src/app/protocols/protocols.page';
 
 @Component({
   selector: 'app-experiments',
@@ -21,10 +21,9 @@ export class ExperimentsPage implements OnInit {
 
   infoAboutMe : any;
 
-  creatorID='';
-  
-  //protocolID='';
   items: Item[] = [];
+
+  displayList: Item[] = [];
 
   newItem: Item = <Item>{};
 
@@ -48,18 +47,20 @@ export class ExperimentsPage implements OnInit {
       this.ApiService.showLoading();
               console.log(myID);
               let experimentToCreate = {
-                "user_notes": this.experimentCredentials.myNotes,
+                "user_notes": '{}',
                 "step_num": this.experimentCredentials.stepNum,
-                "protocol_used": this.experimentCredentials.protUsed
+                "protocol_used": this.experimentCredentials.protUsed,
+                "current_interaction": '{}'
               }
   
               this.ApiService.createExperiment(experimentToCreate).subscribe((res) => {
                 if (res) {
                   console.log(res)
+                  this.loadItems();
                 }
                 else {
                   this.ApiService.stopLoading();
-                  this.ApiService.showError("An error occured while creating a Protocol")
+                  this.ApiService.showError("An error occured while creating a Experiment")
                 }
               });
             //}
@@ -77,7 +78,7 @@ export class ExperimentsPage implements OnInit {
   }
   
   redirect(item) {
-    this.inAppBrowser.create("http://127.0.0.1:8000/imageInteraction/?id=" + item.protocol_used + "&exp=" +item.id);
+    this.inAppBrowser.create("http://35.153.231.217:8000/imageInteraction/?id=" + item.protocol_used + "&exp=" +item.id);
    }
   
   onSelect(item: Item): void {
@@ -88,8 +89,16 @@ export class ExperimentsPage implements OnInit {
 
   loadItems(){
     this.ApiService.getExperiments().subscribe(items => {
-      this.items = items["results"];
-	  console.log(items["results"]);
+      for (let index in items["results"]){
+        let currentItem = items["results"][index]["protocol_used"];
+        //console.log(currentItem)
+		 	  if(currentItem == protocolID){
+          console.log(currentItem)
+				  this.displayList.push(items["results"][index]);
+        }
+    }
+    console.log(this.displayList)
+	  //console.log(items["results"][3]["protocol_used"]);
     });
   }
 
