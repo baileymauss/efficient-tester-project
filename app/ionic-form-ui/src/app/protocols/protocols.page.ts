fc5609969df724b810d44169f6250a5e68eb4664
Bailey Mauss <59042705+baileymauss@users.noreply.github.com>
2021-04-08 12:51:39 -0700

    Update protocols.page.ts

--- app/ionic-form-ui/src/app/protocols/protocols.page.ts
+++ app/ionic-form-ui/src/app/protocols/protocols.page.ts
@@ -5,6 +5,9 @@ import { Platform, ToastController, IonList} from '@ionic/angular';
 import { StorageService, Item } from 'src/app/services/storage.service';
 import { ApiDjangoService } from '../services/api-django.service';
 import {myID} from 'src/app/services/authentication.service';
+import {currentGroup} from 'src/app/lab-group/lab-group.page';
+
+export var protocolID:number;
 
 @Component({
   selector: 'app-protocols',
@@ -56,6 +59,7 @@ export class ProtocolsPage implements OnInit{
               this.ApiService.createProtocol(protocolToCreate).subscribe((res) => {
                 if (res) {
                   console.log(res)
+                  this.loadItems();
                 }
                 else {
                   this.ApiService.stopLoading();
@@ -75,13 +79,16 @@ export class ProtocolsPage implements OnInit{
       //});
     }
   }
+
   
   redirect() {
     this.router.navigateByUrl("/experiments");
    }
   
   onSelect(item: Item): void {
-	  this.selectedItem = item;
+    this.selectedItem = item;
+    protocolID = item.id;
+    console.log(protocolID)
 	  this.redirect();
   }
   
