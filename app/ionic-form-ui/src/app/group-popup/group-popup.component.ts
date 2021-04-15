4cf241f7366cd7f5c9e5ecb4359cdf3acc3a5153
Bailey Mauss <59042705+baileymauss@users.noreply.github.com>
2021-04-15 16:38:10 -0700

    Update group-popup.component.ts
    checking admin status for adding members

--- app/ionic-form-ui/src/app/group-popup/group-popup.component.ts
+++ app/ionic-form-ui/src/app/group-popup/group-popup.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import {AddgroupmemberComponent} from 'src/app/addgroupmember/addgroupmember.component';
+import {myID} from 'src/app/services/authentication.service';
+import { currentGroup } from 'src/app/lab-group/lab-group.page';
+import { ApiDjangoService } from '../services/api-django.service';
 
 @Component({
   selector: 'app-group-popup',
@@ -11,13 +14,34 @@ export class GroupPopupComponent implements OnInit {
 	
 	groupID;
 	
-	constructor(private modalController: ModalController) { }
+	constructor(private modalController: ModalController,
+	private ApiService: ApiDjangoService) { }
 
     async close(){
 	  await this.modalController.dismiss();
 	}
 	
+	checkAdmin(userID, groupID): boolean {
+		this.ApiService.getLabGroupMembership().subscribe(items => {
+			for (let index in items["results"]) {
+				if (items["results"][index]["user"] == userID) {
+					if (items["results"][index]["group"] == groupID) {
+						if (items["results"][index]["role"] == 1) {
+							return true;
+
+						} else {
+							return false;
+						}
+					}
+				}
+
+			}
+		});
+		return false;
+	}
+	
   async addMember(){
+	  if(this.checkAdmin(myID, currentGroup) == true){
 	  const modal = await this.modalController.create({
 		  component: AddgroupmemberComponent,
 		  componentProps: {
@@ -25,6 +49,9 @@ export class GroupPopupComponent implements OnInit {
 		  }
 	  })
 	  await modal.present();
+  } else {
+	  this.ApiService.showError("You cannot add members to this group as you are not an admin!")
+  }
   }
 	
 	
